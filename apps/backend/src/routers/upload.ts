968c3f674155abd8c0fbb127c5973d27b612f232
imoken777 <s1f102301640@iniad.org>
2024-06-17 22:57:14 +0900

    feat: call imageToFoods in uploadRoute

--- apps/backend/src/routers/upload.ts
+++ apps/backend/src/routers/upload.ts
@@ -1,13 +1,38 @@
 import { zValidator } from '@hono/zod-validator'
 import { Hono } from 'hono'
+import type { AppBindings } from '..'
+import { imageToFoods } from '../imageToFoods'
 import { uploadReqSchema } from '../schemas/uploadSchema'
 
-const uploadRouter = new Hono()
+const uploadRouter = new Hono<{ Bindings: AppBindings }>()
 
-uploadRouter.post('/', zValidator('param', uploadReqSchema), async (c) => {
-  const { file } = c.req.valid('param')
-
-  return c.json({ message: 'File uploaded', image: file }, 200)
+const uploadRoute = uploadRouter.post('/', zValidator('json', uploadReqSchema), async (c) => {
+  try {
+    const { file } = c.req.valid('json')
+    if (!file) {
+      return c.json(
+        {
+          error: 'No file found',
+        },
+        400,
+      )
+    }
+    const foods = await imageToFoods(file, c.env)
+    return c.json(
+      {
+        foods: foods,
+      },
+      200,
+    )
+  } catch (e) {
+    return c.json(
+      {
+        error: e,
+      },
+      500,
+    )
+  }
 })
 
+export type UploadRouteType = typeof uploadRoute
 export { uploadRouter }
