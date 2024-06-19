883864dc808c53240f2696e58d0e9a2179a50e8c
mst-mkt <131662659+mst-mkt@users.noreply.github.com>
2024-06-19 18:59:51 +0900

    add(frontend): styling for layout of app pages

--- apps/frontend/src/routes/_app.tsx
+++ apps/frontend/src/routes/_app.tsx
@@ -1,15 +1,17 @@
 import { Outlet, createFileRoute } from '@tanstack/react-router'
+import { Footer } from '../components/ui/Footer'
+import { Header } from '../components/ui/Header'
 
 export const Route = createFileRoute('/_app')({
   component: () => <AppLayout />,
 })
 
 const AppLayout = () => (
-  <div>
-    <header>header</header>
-    <main>
+  <div className="flex min-h-svh flex-col gap-y-12">
+    <Header />
+    <main className="mx-auto w-full max-w-[600px] grow">
       <Outlet />
     </main>
-    <footer>footer</footer>
+    <Footer />
   </div>
 )
