1d4ec9a10237bbd66dd3e6a774b075535bac5688
mst-mkt <131662659+mst-mkt@users.noreply.github.com>
2024-06-19 15:09:29 +0900

    chore(frontend): introduce tailwindcss

--- apps/frontend/src/routes/index.lazy.tsx
+++ apps/frontend/src/routes/index.lazy.tsx
@@ -2,6 +2,7 @@ import { createLazyFileRoute } from '@tanstack/react-router'
 import type { HonoRoutes } from 'backend/src/index'
 import { hc } from 'hono/client'
 import { type ChangeEvent, useState } from 'react'
+import './index.css'
 
 export const Route = createLazyFileRoute('/')({
   component: () => <Home />,
