edac6d4aa87fbaefd83f6f2bd837d833477e51bd
mst-mkt <131662659+mst-mkt@users.noreply.github.com>
2024-06-21 03:32:17 +0900

    fix(frontend): change ButtonComponent to LinkButtonComponent

---
+++ apps/frontend/src/components/common/LinkButton.tsx
@@ -0,0 +1,11 @@
+import { Link, type LinkProps } from '@tanstack/react-router'
+import type { FC } from 'react'
+
+export const LinkButton: FC<LinkProps> = ({ children, ...props }) => (
+  <Link
+    {...props}
+    className="flex w-full items-center justify-center rounded bg-[#6c8] px-4 py-2 font-bold text-white shadow transition-colors disabled:bg-[#8b9] hover:bg-[#5b7]"
+  >
+    {children}
+  </Link>
+)
