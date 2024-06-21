5367fd8b959d63c6f91d7e58987e00461cd0de10
mst-mkt <131662659+mst-mkt@users.noreply.github.com>
2024-06-21 21:45:43 +0900

    chore: change project name (`monthly-vol7` -> `chefcam`)

--- apps/frontend/src/components/layout/Header.tsx
+++ apps/frontend/src/components/layout/Header.tsx
@@ -5,7 +5,7 @@ export const Header = () => (
     <div className="mx-auto flex max-w-[600px] items-center gap-y-4 font-bold text-2xl ">
       <h1 className="grow">ChefCam.</h1>
       <a
-        href="https://github.com/mst-mkt/monthly-vol7"
+        href="https://github.com/mst-mkt/chefcam"
         className="w-fit rounded-md p-2 transition-colors hover:bg-gray-200"
       >
         <IconBrandGithubFilled size={20} />
