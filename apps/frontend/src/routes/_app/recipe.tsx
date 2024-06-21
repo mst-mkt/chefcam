726d1c3e67acb4510a88409ead35d3c87f9310f1
mst-mkt <131662659+mst-mkt@users.noreply.github.com>
2024-06-22 00:21:18 +0900

    add(frontend): data fetch and main login in `/recipe`

--- apps/frontend/src/routes/_app/recipe.tsx
+++ apps/frontend/src/routes/_app/recipe.tsx
@@ -1,5 +1,7 @@
+import { IconLoader2 } from '@tabler/icons-react'
 import { createFileRoute } from '@tanstack/react-router'
 import { z } from 'zod'
+import { apiClient } from '../../lib/apiClient'
 
 const recipeSearchSchema = z.object({
   foods: z.array(z.string()).catch([]),
@@ -7,19 +9,37 @@ const recipeSearchSchema = z.object({
 
 export const Route = createFileRoute('/_app/recipe')({
   validateSearch: (search) => recipeSearchSchema.parse(search),
+  loaderDeps: ({ search: { foods } }) => ({ foods }),
+  loader: async ({ deps: { foods } }) => {
+    const res = await apiClient.recipes.$get({ query: { ingredients: foods } })
+    const data = await res.json()
+    return data
+  },
   component: () => <Recipe />,
+  pendingComponent: () => <Pending />,
 })
 
+const Pending = () => (
+  <div className="flex flex-col items-center gap-y-4 py-8">
+    <IconLoader2 size={64} color="#486" className="animate-spin" />
+    <p>レシピを検索中</p>
+  </div>
+)
+
 const Recipe = () => {
-  const { foods } = Route.useSearch()
+  const recipes = Route.useLoaderData()
 
   return (
-    <div>
-      <ul>
-        {foods.map((food) => (
-          <li key={food}>{food}</li>
+    <>
+      <h2>レシピ一覧</h2>
+      <div>
+        {recipes.map((recipe) => (
+          <div key={recipe.recipeTitle}>
+            <h3>{recipe.recipeTitle}</h3>
+            <p>{recipe.ingredients.join('、')}</p>
+          </div>
         ))}
-      </ul>
-    </div>
+      </div>
+    </>
   )
 }
