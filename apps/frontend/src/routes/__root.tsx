fa93392bcebadee5f0bf27135bc4bbb1e44fb14b
imoken777 <s1f102301640@iniad.org>
2024-06-22 03:01:25 +0900

    refactor: remove TanStackRouterDevtools from root route component

--- apps/frontend/src/routes/__root.tsx
+++ apps/frontend/src/routes/__root.tsx
@@ -1,11 +1,9 @@
 import { Outlet, createRootRoute } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 
 export const Route = createRootRoute({
   component: () => (
     <>
       <Outlet />
-      <TanStackRouterDevtools position="bottom-right" />
     </>
   ),
 })
