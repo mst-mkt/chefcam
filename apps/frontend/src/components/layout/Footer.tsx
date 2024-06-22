09f4c92a6ec07d11925cf397dbe1a81964803844
mst-mkt <131662659+mst-mkt@users.noreply.github.com>
2024-06-22 19:53:39 +0900

    fix(frontend): adjust style for small screen widths

--- apps/frontend/src/components/layout/Footer.tsx
+++ apps/frontend/src/components/layout/Footer.tsx
@@ -1,5 +1,5 @@
 export const Footer = () => (
-  <footer className="mx-auto flex w-full max-w-[600px] items-center gap-x-4 py-4">
+  <footer className="mx-auto flex w-full max-w-[600px] items-center gap-x-4 px-6 py-4">
     <p className="font-bold text-lg">ChefCam.</p>
     <div className="h-[1px] grow bg-gray-400 " />
   </footer>
--- apps/frontend/src/components/layout/Header.tsx
+++ apps/frontend/src/components/layout/Header.tsx
@@ -2,9 +2,9 @@ import { IconBrandGithubFilled } from '@tabler/icons-react'
 import { Link } from '@tanstack/react-router'
 
 export const Header = () => (
-  <header className="sticky top-0 border-b bg-[#fff2] p-4 backdrop-blur-md">
-    <div className="mx-auto flex max-w-[600px] items-center gap-y-4 font-bold text-2xl ">
-      <Link to="/" className="grow">
+  <header className="sticky top-0 border-b bg-[#fff2] backdrop-blur-md">
+    <div className="mx-auto flex max-w-[600px] items-center gap-y-4 px-6 py-4 font-bold text-2xl">
+      <Link to="/" className="grow transition-colors hover:text-[#4c6]">
         ChefCam.
       </Link>
       <a
