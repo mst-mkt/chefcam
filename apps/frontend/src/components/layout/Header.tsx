e33be161c81a5d3938b7347067862d88379ec6b2
imoken777 <s1f102301640@iniad.org>
2024-06-22 18:34:29 +0900

    feat: update Header component h1 to Link for top-page

--- apps/frontend/src/components/layout/Header.tsx
+++ apps/frontend/src/components/layout/Header.tsx
@@ -1,9 +1,12 @@
 import { IconBrandGithubFilled } from '@tabler/icons-react'
+import { Link } from '@tanstack/react-router'
 
 export const Header = () => (
   <header className="sticky top-0 border-b bg-[#fff2] p-4 backdrop-blur-md">
     <div className="mx-auto flex max-w-[600px] items-center gap-y-4 font-bold text-2xl ">
-      <h1 className="grow">ChefCam.</h1>
+      <Link to="/" className="grow">
+        ChefCam.
+      </Link>
       <a
         href="https://github.com/mst-mkt/chefcam"
         className="w-fit rounded-md p-2 transition-colors hover:bg-gray-200"
