d2651c23f78a207cfa758436c54831a785f1ac94
mst-mkt <131662659+mst-mkt@users.noreply.github.com>
2024-06-26 00:31:50 +0900

    refactor(backend): format code and error handling in api handler ( `/upload`)

--- apps/backend/src/routers/upload.ts
+++ apps/backend/src/routers/upload.ts
@@ -6,30 +6,17 @@ import { uploadReqSchema } from '../schemas/uploadSchema'
 const uploadRouter = honoFactory
   .createApp()
   .post('/', zValidator('form', uploadReqSchema), async (c) => {
+    const { file } = c.req.valid('form')
+
+    if (file !== null && !(file instanceof File)) {
+      return c.json({ error: 'No file found' }, 400)
+    }
+
     try {
-      const { file } = c.req.valid('form')
-      if (!file) {
-        return c.json(
-          {
-            error: 'No file found',
-          },
-          400,
-        )
-      }
       const foods = await imageToFoods(file, c.env)
-      return c.json(
-        {
-          foods: foods,
-        },
-        200,
-      )
-    } catch (e) {
-      return c.json(
-        {
-          error: e,
-        },
-        500,
-      )
+      return c.json({ foods }, 200)
+    } catch (error) {
+      return c.json({ error }, 500)
     }
   })
 
