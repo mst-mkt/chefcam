0b84c67f0b64534d5da5d885aa116cc68ce7d965
mst-mkt <131662659+mst-mkt@users.noreply.github.com>
2024-06-26 15:31:11 +0900

    fix(frontend): remove unused types

--- apps/backend/src/schemas/ingredientsSchema.ts
+++ apps/backend/src/schemas/ingredientsSchema.ts
@@ -4,6 +4,4 @@ const ingredientsSchema = z.object({
   data: z.array(z.string()).describe('食材のリスト'),
 })
 
-type Ingredient = z.infer<typeof ingredientsSchema>
-
-export { ingredientsSchema, type Ingredient }
+export { ingredientsSchema }
