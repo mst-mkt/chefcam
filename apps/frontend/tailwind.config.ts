434c62549e60e1340d44f70dc8a636b9b35b5457
mst-mkt <131662659+mst-mkt@users.noreply.github.com>
2024-09-04 00:46:08 +0900

    chore: adopt tailwind for file starts-with `.`

--- apps/frontend/tailwind.config.ts
+++ apps/frontend/tailwind.config.ts
@@ -25,7 +25,7 @@ const makeColorPalette = (color: string) => ({
 })
 
 const config = {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}', './src/**/.*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
     extend: {
