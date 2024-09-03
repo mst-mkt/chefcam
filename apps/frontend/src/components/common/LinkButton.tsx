d63d5b73a9c4f152f229c9200b281f9e1db8048c
mst-mkt <131662659+mst-mkt@users.noreply.github.com>
2024-09-04 02:02:25 +0900

    fix(frontend): format by biome

--- apps/frontend/src/components/common/LinkButton.tsx
+++ apps/frontend/src/components/common/LinkButton.tsx
@@ -4,7 +4,7 @@ import type { FC } from 'react'
 export const LinkButton: FC<LinkProps> = ({ children, ...props }) => (
   <Link
     {...props}
-    className="flex w-full items-center justify-center rounded bg-accent-500 px-4 py-2 font-bold text-white shadow transition-colors disabled:bg-[#8b9] hover:bg-[#5b7]"
+    className="flex w-full items-center justify-center rounded bg-accent-500 px-4 py-2 font-bold text-white shadow transition-colors hover:bg-[#5b7] disabled:bg-[#8b9]"
   >
     {children}
   </Link>
