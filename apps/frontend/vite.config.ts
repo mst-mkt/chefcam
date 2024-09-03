8bbc9837f37172a76f4cd22fd372cf80fac30680
mst-mkt <131662659+mst-mkt@users.noreply.github.com>
2024-09-04 04:57:22 +0900

    chore(frontend): add configs for pwa

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,10 +1,47 @@
 import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
     react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['/favicon.svg', '/apple-touch-icon.png'],
+      injectRegister: 'auto',
+      manifest: {
+        name: 'ChefCam.',
+        short_name: 'chefcam',
+        description:
+          'Just take a picture of your refrigerator and you can instantly search for your dinner recipe.',
+        theme_color: '#00ac3d',
+        icons: [
+          {
+            src: 'pwa_192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa_512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa_512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: 'pwa_512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+    }),
     TanStackRouterVite({
       generatedRouteTree: './src/lib/tanstack-router/route-tree.gen.ts',
       routesDirectory: 'src/routes',
