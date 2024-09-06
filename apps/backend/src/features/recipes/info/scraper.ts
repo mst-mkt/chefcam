5726b7b9edeab1faa0d6002c558dc97c110887fb
mst-mkt <131662659+mst-mkt@users.noreply.github.com>
2024-09-07 02:08:36 +0900

    add(backend): scraper and endpoint of recipe-page

---
+++ apps/backend/src/features/recipes/info/scraper.ts
@@ -0,0 +1,114 @@
+import { load } from 'cheerio'
+import { z } from 'zod'
+import { getRecipeIdFromUrl } from './recipeId'
+
+const recipeInfoSchema = z
+  .object({
+    title: z.string().min(1),
+    thumbnail: z.string().url().optional(),
+    description: z.string().min(1).optional(),
+    ingredients: z
+      .array(
+        z.union([
+          z
+            .object({
+              caption: z.string().min(1),
+            })
+            .strict(),
+          z
+            .object({
+              ingredient: z.string().min(1),
+              quantity: z.string().min(1).optional(),
+              recipeId: z.string().min(1).optional(),
+            })
+            .strict(),
+        ]),
+      )
+      .min(1),
+    steps: z
+      .array(
+        z
+          .object({
+            step: z.string().min(1).optional(),
+            images: z.array(z.string().url()).optional(),
+            recipeId: z.string().min(1).optional(),
+          })
+          .strict(),
+      )
+      .min(1),
+    point: z.string().min(1).optional(),
+  })
+  .strict()
+
+export const scrapeRecipePage = async (html: string) => {
+  const $ = load(html)
+  const recipe = $('#recipe')
+
+  const title = recipe.find('h1').text().trim()
+  const thumbnail = recipe.find('.tofu_image > picture > img').attr('src')
+  const description = recipe.find('h1').parent().children().last().find('p').text().trim()
+
+  const ingredients = recipe
+    .find('.ingredient-list > ol')
+    .children()
+    .map((_, ingredient) => {
+      const classNames = ingredient.attribs.class?.split(' ') ?? []
+      const isHeadline = classNames.includes('headline')
+
+      if (isHeadline) {
+        return { caption: $(ingredient).text().trim() }
+      }
+
+      const quantity = $(ingredient).find('bdi').text().trim()
+      const recipeLink = $(ingredient).children('div > a').attr('href')
+
+      return {
+        ingredient: $(ingredient).find('span').text().trim(),
+        quantity: quantity !== '' ? quantity : undefined,
+        recipeId: recipeLink !== undefined ? getRecipeIdFromUrl(recipeLink) : undefined,
+      }
+    })
+    .get()
+
+  const steps = recipe
+    .find('#steps > ol')
+    .children()
+    .map((_, step) => {
+      const text = $(step).find('.w-full > [dir="auto"]').children('p').text().trim()
+      const recipeLink = $(step).find('.w-full > .w-full').find('a').attr('href')
+      const images = $(step)
+        .find('.w-full > [dir="auto"]')
+        .next()
+        .children('a')
+        .map((_, image) => $(image).find('picture > img').attr('src'))
+        .get()
+
+      return {
+        step: text,
+        images,
+        recipeId: recipeLink !== undefined ? getRecipeIdFromUrl(recipeLink) : undefined,
+      }
+    })
+    .get()
+
+  const point = recipe.find('#advice').children('p').text().trim()
+
+  const recipeInfo = {
+    title,
+    thumbnail,
+    description,
+    ingredients,
+    steps,
+    point,
+  }
+
+  const validated = recipeInfoSchema.safeParse(recipeInfo)
+
+  if (!validated.success) {
+    console.error('Invalid recipe:', validated.error.issues)
+    console.table(validated.error.issues)
+    return null
+  }
+
+  return validated.data
+}
