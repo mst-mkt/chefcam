b0476a3363841a2e7c8bfdad669789207e3dd75e
mst-mkt <131662659+mst-mkt@users.noreply.github.com>
2024-09-14 00:30:59 +0900

    add(frontend): login/logout button in header

--- apps/frontend/src/components/layout/Header.tsx
+++ apps/frontend/src/components/layout/Header.tsx
@@ -1,10 +1,13 @@
-import { IconBrightness } from '@tabler/icons-react'
+import { signIn, signOut, useSession } from '@hono/auth-js/react'
+import { IconBrightness, IconDoorEnter, IconDoorExit } from '@tabler/icons-react'
 import { Link } from '@tanstack/react-router'
 import { PROJECT_NAME } from '../../constants/projects'
 import { useTheme } from '../../hooks/useTheme'
+import { IconButton } from '../common/IconButton'
 
 export const Header = () => {
   const { toggleTheme } = useTheme()
+  const { data: session } = useSession()
 
   return (
     <header className="sticky top-0 z-50 border-background-100 border-b bg-backgroung/16 backdrop-blur-md">
@@ -12,14 +15,21 @@ export const Header = () => {
         <Link to="/" className="transition-colors hover:text-accent-400">
           {PROJECT_NAME}
         </Link>
-        <button
-          type="button"
-          onClick={toggleTheme}
-          aria-label="Toggle theme"
-          className="w-fit rounded-md p-2 transition-colors hover:bg-background-200 focus:outline-none"
-        >
-          <IconBrightness size={20} />
-        </button>
+        <div className="flex gap-x-2">
+          <button
+            type="button"
+            onClick={toggleTheme}
+            aria-label="Toggle theme"
+            className="w-fit rounded-md p-2 transition-colors hover:bg-background-100 focus:outline-none"
+          >
+            <IconBrightness size={20} />
+          </button>
+          <IconButton
+            icon={session === null ? IconDoorEnter : IconDoorExit}
+            onClick={() => (session === null ? signIn('google') : signOut())}
+            className="bg-transparent"
+          />
+        </div>
       </div>
     </header>
   )
