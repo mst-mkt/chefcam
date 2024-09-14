638059a15bfa0ebc4c97afe668248a7032464780
mst-mkt <131662659+mst-mkt@users.noreply.github.com>
2024-09-15 04:06:40 +0900

    chore(backend): separate config for remote and local db

--- apps/backend/drizzle.config.ts
+++ apps/backend/drizzle.config.ts
@@ -1,8 +1,19 @@
 import type { Config } from 'drizzle-kit'
 
-export default {
+const remoteConfig = {
   schema: './src/drizzle/schema.ts',
   out: './src/drizzle/migrations/',
   dialect: 'sqlite',
   driver: 'd1-http',
 } satisfies Config
+
+const localConfig = {
+  schema: './src/drizzle/schema.ts',
+  out: './src/drizzle/migrations/',
+  dialect: 'sqlite',
+  dbCredentials: {
+    url: './.wrangler/state/v3/d1/miniflare-D1DatabaseObject/035cee5d9d4111e5318a84d863dfb624ed8cb5bd2da46860deb321a831411ff2.sqlite',
+  },
+} satisfies Config
+
+export default process.env.NODE_ENV === 'production' ? remoteConfig : localConfig
